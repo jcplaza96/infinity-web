11d533d53e691d9faaec4faac8ea06672b07b9fd
Fernan619 <fernan619@bitbucket.com>
2018-11-15 12:50:24 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { ListItemComponent } from './components/list-item/list-item.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SliderIndexComponent } from './components/slider-index/slider-index.component';
+
+const routes: Routes = [
+  { path: 'nosotros', component: ItemListComponent },
+  { path: '', component: SliderIndexComponent , pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
+
 
 @NgModule({
   declarations: [
@@ -20,7 +28,8 @@ import { SliderIndexComponent } from './components/slider-index/slider-index.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
