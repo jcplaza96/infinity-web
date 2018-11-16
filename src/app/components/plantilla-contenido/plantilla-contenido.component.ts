26f6c6fcb7a59531674cd7adb223ace1a6a9d50e
jplagar <juankaplaza@gmail.com>
2018-11-16 14:33:31 +0100

    ignore

--- src/app/components/plantilla-contenido/plantilla-contenido.component.ts
+++ src/app/components/plantilla-contenido/plantilla-contenido.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plantilla-contenido.component.css']
 })
 export class PlantillaContenidoComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
