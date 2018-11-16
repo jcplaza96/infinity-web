0249875500c8bf27f6b4ef1f0bd91d832abb566d
Fernan619 <fernan619@bitbucket.com>
2018-11-16 12:14:58 +0100

    Deploy 6

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/infweb'));
+app.use(express.static(__dirname + '/dist/proyectoDIW/infweb'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/infweb/index.html'));
+res.sendFile(path.join(__dirname+'/dist/proyectoDIW/infweb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
