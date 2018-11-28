0dcd8fd53a0b3ee080d822643310d0c3ba088fa4
Fernan619 <fernan619@bitbucket.com>
2018-11-28 10:48:18 +0100

    Creación servicio

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(
+  ) { }
+}
