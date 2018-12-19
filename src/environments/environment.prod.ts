177588a6d8f0d67fe9769142a6ca98581043c3c1
Fernan619 <fernan619@bitbucket.com>
2018-12-19 11:49:23 +0100

    Cambio a modo produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyANBiy5zA2_-oQUIcH78ZtKMC7T7kPX_YY",
+    authDomain: "infweb-721e9.firebaseapp.com",
+    databaseURL: "https://infweb-721e9.firebaseio.com",
+    projectId: "infweb-721e9",
+    storageBucket: "infweb-721e9.appspot.com",
+    messagingSenderId: "371784615226"
+  }
 };
