4d88d0ee44b6d8c52f2da7507db902f429fb7647
Fernan619 <fernan619@bitbucket.com>
2018-12-19 11:54:36 +0100

    Deploy

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent implements OnInit {
-
+//
   constructor() { }
 
   ngOnInit() {
