6f520c6d6f7c458b4ec33c11f6cdfa05f502bfb9
angelmohi <angelmh1996@gmail.com>
2019-01-24 10:44:34 +0100

    login

--- src/app/components/login-sign/login-sign.component.ts
+++ src/app/components/login-sign/login-sign.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery:any;
+declare var $:any;
+
+
 @Component({
   selector: 'app-login-sign',
   templateUrl: './login-sign.component.html',
   styleUrls: ['./login-sign.component.scss']
 })
 export class LoginSignComponent implements OnInit {
+  target:any;
 
   constructor() { }
 
   ngOnInit() {
+    $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+      var $this = $(this),
+          label = $this.prev('label');
+    
+        if (e.type === 'keyup') {
+          if ($this.val() === '') {
+              label.removeClass('active highlight');
+            } else {
+              label.addClass('active highlight');
+            }
+        } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+            label.removeClass('active highlight'); 
+          } else {
+            label.removeClass('highlight');   
+          }   
+        } else if (e.type === 'focus') {
+          
+          if( $this.val() === '' ) {
+            label.removeClass('highlight'); 
+          } 
+          else if( $this.val() !== '' ) {
+            label.addClass('highlight');
+          }
+        }
+    
+    });
+    
+    $('.tab a').on('click', function (e) {
+      
+      e.preventDefault();
+      
+      $(this).parent().addClass('active');
+      $(this).parent().siblings().removeClass('active');
+      
+      this.target = $(this).attr('href');
+    
+      $('.tab-content > div').not(this.target).hide();
+      
+      $(this.target).fadeIn(600);
+      
+    });
   }
+
+
 
 }
