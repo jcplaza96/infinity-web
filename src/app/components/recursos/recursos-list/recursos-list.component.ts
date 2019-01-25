916779310ca93ec866fc2b9f352fb676fa8a152c
Fernan619 <fernan619@bitbucket.com>
2019-01-25 09:10:42 +0100

    Actualización Bootstrap 4.2 y Carga de contenidos

--- src/app/components/recursos/recursos-list/recursos-list.component.ts
+++ src/app/components/recursos/recursos-list/recursos-list.component.ts
@@ -7,7 +7,8 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./recursos-list.component.scss']
 })
 export class RecursosListComponent implements OnInit {
-
+  carga;contenido;
+  
   items: any;
   id: string;
   constructor(private conexion: FirebaseService) {
@@ -18,9 +19,19 @@ export class RecursosListComponent implements OnInit {
   }
   
   ngOnInit() {
+    this.carga = document.getElementById("carga");
+    this.contenido = document.getElementById("contenido");
     this.conexion.listaItem('recursos').subscribe(item =>{
       this.items = item;      
     })
     
+  }
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    setTimeout(()=>{
+      this.contenido.className = "container";
+      this.carga.className = "d-none";    
+    },1200)
   }
 }
