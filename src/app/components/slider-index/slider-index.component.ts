764c174ab8463c1a0409ba7874c1c52576cec28f
angelmohi <angelmh1996@gmail.com>
2019-01-30 08:55:20 +0100

    slider deslizar en movil

--- src/app/components/slider-index/slider-index.component.ts
+++ src/app/components/slider-index/slider-index.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { OfertasSliderComponent } from '../ofertas-slider/ofertas-slider.component';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-slider-index',
@@ -24,6 +26,25 @@ export class SliderIndexComponent implements OnInit {
     this.conexion.listaItem(this.parent).subscribe(item =>{
       this.items = item;
     })
+    this.deslizarSlider();
+  }
+
+  deslizarSlider(){
+    $(".carousel").on("touchstart", function(event){
+      var xClick = event.originalEvent.touches[0].pageX;
+      $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(this).carousel('prev');
+        }
+      });
+      $(".carousel").on("touchend", function(){
+        $(this).off("touchmove");
+      });
+    });
   }
 
 }
