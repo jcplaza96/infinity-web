c9ae98b7db75e837eeae9b848898fb5484f59536
Fernan619 <fernan619@bitbucket.com>
2019-02-13 10:44:13 +0100

    Rating visual

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,6 +1,7 @@
 import { FirebaseStorageService } from './../../services/storage/firebase-storage.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Item } from 'src/app/services/firebase.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-list-item',
@@ -8,16 +9,27 @@ import { Item } from 'src/app/services/firebase.service';
   styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent implements OnInit {
-
+  radio: HTMLFormElement[];
   @Input() i: Item;
   @Input() id: string;
   
+
   shortDescription: string = "";
   seeMore: boolean = false;
   constructor(private fbs: FirebaseStorageService) {
   }
   
   ngOnInit() {
+    this.getShortDescription();
+    //console.log(document.getElementsByName("rating"));
+
+   }
+
+  toogleSeeMore(){
+    this.seeMore = !this.seeMore;
+  }
+
+  getShortDescription(){
     let desc = this.i.description.split(" ");
     //console.log(desc);
     for (let index = 0; index < 13 && desc[index] != null; index++) {
@@ -26,7 +38,7 @@ export class ListItemComponent implements OnInit {
     if(desc.length > 13) this.shortDescription = this.shortDescription.concat("...");
   }
 
-  toogleSeeMore(){
-    this.seeMore = !this.seeMore;
+  uRate(f: NgForm){
+    console.log(f.controls['rating'].value);
   }
 }
